385d602d1687e091a73f82c7ce3931e47f05c017
jkp09x <jkp09x@users.noreply.github.com>
2020-06-09 22:19:25 -0400

    Different ways to access elements in JS

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ document.firstElementChild.lastElementChild.querySelector("input").click();
 document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Jigar";
 //    (instructor's solution)
 document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML = "Angela"
+
+// different way to access html elements
+document.getElementsByTagName("li"); // OUTPUT: HTMLCollection(3) [li.list, li.list, li.list]
+document.getElementsByTagName("li")[1].style.color = "blue"; // Set color of middle li to blue
+document.getElementsByClassName("header_hello"); // OUTPUT: HTMLCollection [h1.header_hello]
+
+// Using querySelector
+document.querySelector("a"); // OUTPUT: <a href=​"https:​/​/​www.google.com">​Google​</a>​
+document.querySelector("li a"); // OUTPUT: <a href=​"https:​/​/​www.google.com">​Google Site​</a>​
