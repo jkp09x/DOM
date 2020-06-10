4f683b6020ba5ad7b07dbd737ca772cb8634dcd3
jkp09x <jkp09x@users.noreply.github.com>
2020-06-09 22:03:13 -0400

    Changing a specific element via the DOM

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 alert("Hello World");
 
 document.firstElementChild.lastElementChild.querySelector("input").click();
+
+// changing the third li element to my name
+//    (my solution)
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Jigar";
+//    (instructor's solution)
+document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML = "Angela"
